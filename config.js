d4bdc1dae6837c41f2a2eddeb16a9ce117c425fc
demoaccountf <153440568+demoaccountf@users.noreply.github.com>
2024-01-20 16:19:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ var config = {
         "Ở hơi xa mà còn trái múi giờ nữa nên hơi khó nhỉ?",
         "Nhưng không sao",
         "là một coder nửa mùa, chỉ có thể gửi cái này thôi",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
-        "要把我家可爱猪猪喂饱饱",
-        "然后抱走",
-        "YAMI~~",
+        "Hi vọng lần tới có thể đón sinh nhật cùng nhau nhá",
+        "Có toi ở đây rồi nên đừng một mình nữa nhé!",
+        "Best wishes for you",
+        "i hope you get everything you want",
+        "Wishing my sweetie a happy birthday",
+        "From Banhb3o with love",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -25,7 +25,7 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/ngoc.png",
+        "心爱的小可爱": "./imgs/ngoc.jpg",
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
